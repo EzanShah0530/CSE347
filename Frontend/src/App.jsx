64ec870813c6c68513e827669eeae4eca37d1dba
Shah Abdul Mazid <ezanshah58@gmail.com>
2024-02-21 13:23:18 +0600

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import React  from 'react'
 import './App.css'
 import Login from './Pages/Login'
-import img1 from './images/Software-Developments-HMS-Hospital-Management-System.webp'
+//import img1 from './images/Software-Developments-HMS-Hospital-Management-System.webp'
 
 function App() {
   return (
     <>
-    <img src={img1} alt=''/>
     </>
   )
 }
