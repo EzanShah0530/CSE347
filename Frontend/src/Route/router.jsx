cb79e3b58707888d69e8f917e6124430fd55b0a9
Shah Abdul Mazid <ezanshah58@gmail.com>
2024-02-24 22:15:58 +0600

    commmit

--- Frontend/src/Route/router.jsx
+++ Frontend/src/Route/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App.jsx";
 import Login from "../Pages/Login.jsx";
-import Register from "../Pages/Reg.jsx";
+import Register from "../Pages/Register.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
