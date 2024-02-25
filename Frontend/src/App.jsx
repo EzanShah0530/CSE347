bdf55b337c69ede143edff5d387e2a8fa0c56d9f
Shah Abdul Mazid <ezanshah58@gmail.com>
2024-02-25 11:54:55 +0600

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React ,{useEffect} from 'react';
 import './App.css'
 
-function App() {
+export default function App() { 
+  useEffect(()=>{
+  document.title='ESMB Healthcare Trust';},[]);
   return (
+    <>
     <div className="app-container">
       <div className="content">
-        
+        <h1 className='title'>Welcome To ESMB Healthare Trust</h1>
       </div>
       <footer className="footer">
         <marquee className="custom-marquee">
-          <h1> Developed By Shah Abdul Mazid, Jannatul Ferdouse,
-            Sabrina Akter & Budrun Nahar Bristy</h1>
+          <h1> Developed By Shah Abdul Mazid,Jannatul Ferdouse,Sabrina Akter & Budrun Nahar Bristy</h1>
         </marquee>
+        <h1></h1>
       </footer>
-    </div>    
+    </div>  
+    </>  
   )
 }
-export default App;
