da2e586e3e12802b686f4304c5a4deadbd700536
Shah Abdul Mazid <ezanshah58@gmail.com>
2024-03-16 23:38:31 +0600

    commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,10 @@
 import React ,{useEffect} from 'react';
+import { useState } from 'react';
+import './Route/router.jsx';
 import './App.css'
 
 export default function App() { 
+  const [showDropdown, setShowDropdown] = useState(false);
   useEffect(()=>{
   document.title='ESMB Healthcare Trust';},[]);
   return (
@@ -10,11 +13,24 @@ export default function App() {
       <div className="content">
         <h1 className='title'>Welcome To ESMB Healthare Trust</h1>
       </div>
+      <div className="dropdown">
+        <div className="buttons-container">
+          <button className="home" onClick={() => setShowDropdown(false)}>Home</button>
+          <button className="aboutus" onClick={() => setShowDropdown(!showDropdown)}>About us</button>
+        </div>
+        {showDropdown && (
+          <div className="dropdown-content">
+            <a href="/areg">Link1</a>
+            <a href="/alogin">Link2</a>
+            <a href="#">Link3</a>
+          </div>
+        )}
+      </div>
+
       <footer className="footer">
         <marquee className="custom-marquee">
           <h1> Developed By Shah Abdul Mazid,Jannatul Ferdouse,Sabrina Akter & Budrun Nahar Bristy</h1>
         </marquee>
-        <h1></h1>
       </footer>
     </div>  
     </>  
